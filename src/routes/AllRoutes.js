18caa3c22103d9f0e5c30a744660c33b48eab3bd
bhaveshlande <bhavesh.baliram-lande@capgemini.com>
2023-11-01 17:40:11 +0530

    git pages added

--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,9 +1,9 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import { PageNotFound, ChuckNorriesJokesPage, WordCounterHome } from "../pages";
 export const AllRoutes = () => {
   return (
     <>
-      <Router>
+      <Router basename="/">
         <Routes>
           <Route path="/" element={<WordCounterHome />} />
           <Route
